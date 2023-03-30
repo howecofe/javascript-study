189a3b37af6bdaf5ef18e133b5e82d759114dba9
howecofe <am0229@naver.com>
2023-03-31 01:53:34 +0900

    제어문

---
+++ ch3_control/7.logical.js
@@ -0,0 +1,26 @@
+// 7. 논리 연산자 (Logical operators)
+// && 그리고
+// || 또는
+// ! 부정 (단항연산자에서 온 것)
+// !! boolean 값으로 변환 (단항연산자 응용버전)
+let num = 8;
+if (num >= 0 || num > 20) {
+  console.log('👍');
+}
+
+if (num != 8) {
+  console.log('🙏🏻');
+}
+
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log(!'text'); // false
+console.log(!!'text'); // true
